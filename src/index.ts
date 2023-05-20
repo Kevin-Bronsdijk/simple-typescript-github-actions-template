bd29e6beb5e32aad01bd4d04500b9b85734020a0
Kevin Bronsdijk <Kevin-Bronsdijk@users.noreply.github.com>
2023-05-20 09:40:03 +0700

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
 import * as core from '@actions/core'
-import {checkForEmptyTags, checkForMissingTags} from './helpers'
 
 async function run(): Promise<void> {
   try {
     const fullPath = core.getInput('full-path')
     core.debug(`fullPath: ${fullPath}`)
 
-    const hasEmptyTags = checkForEmptyTags(fullPath)
-    const hasMissingTags = checkForMissingTags(fullPath)
+    core.debug(`debug`)
 
-    core.debug(
-      `hasEmptyTags: ${hasEmptyTags} - hasMissingTags: ${hasMissingTags}`
-    )
-
-    core.setOutput('found-missing-translations', hasEmptyTags && hasMissingTags)
+    core.setOutput('x', 'y')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
